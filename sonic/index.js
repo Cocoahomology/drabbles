09890af236ff7d0ad6eef81addf28239b811b2d1
Cocoahomology <83151536+Cocoahomology@users.noreply.github.com>
2022-03-22 19:18:02 -0300

    Update index.js

--- sonic/index.js
+++ sonic/index.js
@@ -1,12 +1,15 @@
 const { SwapCanisterController } = require("@psychedelic/sonic-js");
-
 const retry = require("async-retry");
 const axios = require("axios");
 const { toUSDTBalances } = require("../helper/balances");
 
+poolIDs = [
+  "aanaa-xaaaa-aaaah-aaeiq-cai", //XTC-WICP Pool
+];
+
 const anonymousController = new SwapCanisterController();
 
-const fetchIcpPrice = async () => {
+const fetchICPPrice = async () => {
   const res = await axios.get(
     "https://api.coingecko.com/api/v3/simple/price?ids=internet-computer&vs_currencies=usd"
   );
@@ -14,24 +17,40 @@ const fetchIcpPrice = async () => {
 };
 
 const fetch = async () => {
-  const pool = await anonymousController.getPairList();
-  let {
-    ["aanaa-xaaaa-aaaah-aaeiq-cai"]: {
-      ["utozz-siaaa-aaaam-qaaxq-cai"]: {
+  var poolBalances = [];
+
+  const poolList = await anonymousController.getPairList();
+
+  for (const pool in poolList) {
+    if (poolIDs.includes(pool)) {
+      let {
+        ["token0"]: token0,
+        ["token1"]: token1,
         ["reserve0"]: bigIntReserve0,
-        ["reserve1"]: bigIntReserve1,
-      },
-    },
-  } = pool;
+        ["reserve1"]: bigIntReserve1
+      } = Object.values(poolList[pool])[0];
 
-  const reserve0 = Number(bigIntReserve0) / 10 ** 12; // XTC token: 12 decimals
-  const reserve1 = Number(bigIntReserve1) / 10 ** 8; // wrappedICP token: 8 decimals
+      poolBalances.push({
+        ["token0"]: token0,
+        ["token1"]: token1,
+        ["bigIntReserve0"]: bigIntReserve0,
+        ["bigIntReserve1"]: bigIntReserve1
+      });
+    }
+  }
 
-  const icpPrice = await fetchIcpPrice();
+  /*
+   * below should be updated once there is way to deal with decimals/prices of different tokens on ICP
+   */
 
-  const xtcWicpPoolValue = 2 * reserve1 * icpPrice; // XTC not yet on other DEXes, so value of pool taken to be twice ICP value
+  const reserve0 = Number(poolBalances[0].bigIntReserve0) / 10 ** 12; // XTC token: 12 decimals
+  const reserve1 = Number(poolBalances[0].bigIntReserve1) / 10 ** 8; // wrappedICP token: 8 decimals
 
-  const tvl = xtcWicpPoolValue;
+  const ICPPrice = await fetchICPPrice();
+
+  const XTCWICPPoolValue = 2 * reserve1 * ICPPrice; // XTC not yet on other DEXes, so value of pool taken to be twice ICP value
+
+  const tvl = XTCWICPPoolValue;
 
   return toUSDTBalances(tvl);
 };
